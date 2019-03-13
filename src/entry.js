d9ecc4c2da399aa4c2966bdd4856086f49e911ce
xuan45 <315764194@qq.com>
2019-03-13 23:19:59 +0800

    fix: #28

--- src/entry.js
+++ src/entry.js
@@ -1,9 +1,26 @@
 /* eslint-disable */
+import url from 'url';
+import SockJS from 'sockjs-client';
 import {
   setEditorHandler,
+  reportBuildError,
   startReportingRuntimeErrors,
 } from 'react-error-overlay'
 import launchEditorEndpoint from 'react-dev-utils/launchEditorEndpoint'
+import formatWebpackMessages from 'react-dev-utils/formatWebpackMessages';
+
+
+
+const connection = new SockJS(
+  url.format({
+    protocol: window.location.protocol,
+    hostname: window.location.hostname,
+    port: window.location.port,
+    // Hardcoded in WebpackDevServer
+    pathname: '/sockjs-node',
+  })
+);
+
 
 setEditorHandler(errorLocation => {
   // Keep this sync with errorOverlayMiddleware.js
@@ -29,3 +46,18 @@ startReportingRuntimeErrors({
     }
   },
 })
+
+connection.onmessage = function(e) {
+  const {type, data} = JSON.parse(e.data);
+  switch (type) {
+    case 'errors':
+      const {errors} = formatWebpackMessages({
+        errors: data,
+        warnings: [],
+      });
+      reportBuildError(errors[0]);
+      break;
+    default:
+    // Do nothing.
+  }
+};
